07aa4944ace6065d3a82e699e7cc8f3e235080eb
MrCrashocaster <mrcrashocaster@gmail.com>
2022-03-06 21:33:28 -0600

    Updated site.

--- js/site.js
+++ js/site.js
@@ -18,16 +18,43 @@ $(document).ready(function() {
     // WINDOW SCROLL
     $(window).on('scroll', function(){
         scrollTop = $(window).scrollTop();
+        
+        var current = $('.changeBackground');
+        if (current.visible( true )) {
+            $('body').addClass('background-transition');
+            $('body').removeClass('background-transition-out');
+            $('body').addClass('bg-black');
+            $('body').removeClass('bg-white');
+            $('p').addClass('white');
+            $('.h2').addClass('white');
+        }
+        else if (!current.visible( true )) {
+            $('body').addClass('background-transition-out');
+            $('body').addClass('bg-white');
+            $('body').removeClass('background-transition');
+            $('body').removeClass('bg-black');
+            $('p').removeClass('white');
+            $('.h2').removeClass('white');
+        }
+        
+        if ($(window).width() >= 768) {
+            $('.fade-up, .fade-down, .fade-right, .fade-left').each(function() {
+                var crrnt = $(this);
+                if (crrnt.visible( true )) {
+                    crrnt.addClass('show');
+                }
+                else if (!crrnt.visible( true )) {
+                    crrnt.removeClass('show');
+                }
+            });  
+        }
 
-        $('.fade-up, .fade-down, .fade-right, .fade-left').each(function() {
-            var crrnt = $(this);
-            if (crrnt.visible( true )) {
+        if ($(window).width() <= 768) {
+            $('.fade-up, .fade-down, .fade-right, .fade-left').each(function() {
+                var crrnt = $(this); 
                 crrnt.addClass('show');
-            }
-            else if (!crrnt.visible( true )) {
-                crrnt.removeClass('show');
-            }
-        });
+            });  
+        }
         
     });
 });
--- packages-page.php
+++ packages-page.php
@@ -50,17 +50,17 @@ get_header(); ?>
             <div id="unparalledService" class="">
                 <div class="wrapper1200">
                     <div class="unparalledServiceContent">
-                        <div class="unparalledCopy fade-down">
-                            <div class="h2"><span class="underlined-word">Unparalled <div class="underline"></span></div></div>
-                            <div class="h2 dark"> Service</div>
-                            <p>Vestibulum ac parturient vitae parturient elit suspendisse nec 
+                        <div class="unparalledCopy">
+                            <div class="h2 fade-right"><span class="underlined-word">Unparalled <div class="underline"></span></div></div>
+                            <div class="h2 fade-right"> Service</div>
+                            <p class="fade-right">Vestibulum ac parturient vitae parturient elit suspendisse nec 
                                 fringilla torquent ridiculus rhoncus nibh tellus condimentum. 
                                 Conubia dictumst suscipit pharetra habitant sagittis netus pulvinar 
                                 nascetur ut sociis a sagittis a suspendisse parturient duis nullam a velit viverra. 
                                 Sodales etiam tempus tempor a vestibulum nascetur ullamcorper sociosqu condimentum 
                                 aliquam facilisi mi gravida est et habitant.</p>
 
-                                <div class="button">
+                                <div class="button fade-right">
                                     <a href="/about">
                                         <div class="btn">
                                             <p>About</p>
@@ -69,12 +69,116 @@ get_header(); ?>
                                     </a>
                                 </div>
                         </div>
-                        <div class="unparalledVideo fade-up">
+                        <div class="unparalledVideo fade-left">
                             <video autoplay="autoplay" playsinline loop="loop" muted="muted">
                                 <source src="https://trilakeguideservice.com/wp-content/uploads/2022/03/TriLake-B-Roll-Min.mp4" type="video/mp4">
                             </video>
                         </div>
                     </div> 
+                </div>
+            </div>
+            <div id="packageSection" class="">
+                <div class="wrapper1200">
+                    <div class="packagesHeadline">
+                        <div class="h1 red fade-right">Packages</div>
+                        <div class="h2 fade-right">Grab your adventure</div>
+                        <p class="fade-right">Vestibulum ac parturient vitae parturient elit suspendisse nec 
+                            fringilla torquent ridiculus rhoncus nibh tellus condimentum.
+                        </p>
+                    </div>
+                    <div class="packages">
+                        <div class="singlePackage fade-right">
+                            <div class="singlePackageContent">
+                                <div class="h2"><span class="underlined-word">Package 1<div class="underline"></span></div></div>
+                                <div class="h2 dark subtext"> For the beginner</div>
+                                <p class="dark">Vestibulum ac parturient vitae parturient elit suspendisse nec 
+                                    fringilla torquent ridiculus rhoncus nibh tellus condimentum.
+                                </p>
+                                <div class="button">
+                                    <a href="/about">
+                                        <div class="btn">
+                                            <p class="dark">Inquiry</p>
+                                            <div class="btnImg"></div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="singlePackage fade-left">
+                            <div class="singlePackageContent">
+                                <div class="h2"><span class="underlined-word">Package 2<div class="underline"></span></div></div>
+                                <div class="h2 dark subtext"> For the intermediate</div>
+                                <p class="dark">Vestibulum ac parturient vitae parturient elit suspendisse nec 
+                                    fringilla torquent ridiculus rhoncus nibh tellus condimentum.
+                                </p>
+                                <div class="button">
+                                    <a href="/about">
+                                        <div class="btn">
+                                            <p class="dark">Inquiry</p>
+                                            <div class="btnImg"></div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="singlePackage fade-right">
+                            <div class="singlePackageContent">
+                                <div class="h2"><span class="underlined-word">Package 3<div class="underline"></span></div></div>
+                                <div class="h2 dark subtext"> For the enthusiast</div>
+                                <p class="dark">Vestibulum ac parturient vitae parturient elit suspendisse nec 
+                                    fringilla torquent ridiculus rhoncus nibh tellus condimentum.
+                                </p>
+                                <div class="button">
+                                    <a href="/about">
+                                        <div class="btn">
+                                            <p class="dark">Inquiry</p>
+                                            <div class="btnImg"></div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="singlePackage fade-left">
+                            <div class="singlePackageContent">
+                                <div class="h2"><span class="underlined-word">Package 4<div class="underline"></span></div></div>
+                                <div class="h2 dark subtext"> For the expert</div>
+                                <p class="dark">Vestibulum ac parturient vitae parturient elit suspendisse nec 
+                                    fringilla torquent ridiculus rhoncus nibh tellus condimentum.
+                                </p>
+                                <div class="button">
+                                    <a href="/about">
+                                        <div class="btn">
+                                            <p class="dark">Inquiry</p>
+                                            <div class="btnImg"></div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="ctaContact" style="background: url('https://trilakeguideservice.com/wp-content/uploads/2021/12/pexels-cottonbro-4828253-min-e1646622391180.jpg') center top no-repeat; background-size: cover; width: 100%;">
+                <div class="wrapper1200">
+                    <div class="card">
+                        <div class="cardContent">
+                            <div class="h2"><span class="underlined-word">Contact<div class="underline"></span></div></div>
+                            <div class="h2"> Us</div>
+                                <p class="dark">Vestibulum ac parturient vitae parturient elit suspendisse nec 
+                                    fringilla torquent ridiculus rhoncus nibh tellus condimentum.
+                                </p>
+                                <div class="button">
+                                    <a href="/contact">
+                                        <div class="btn">
+                                            <p class="dark">Contact</p>
+                                            <div class="btnImg"></div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
