908d6931ea394c7cc0a8d61ffb709bc2fbcc4479
MrCrashocaster <mrcrashocaster@gmail.com>
2022-03-12 22:54:15 -0600

    Updated site.

--- page-contact.php
+++ page-contact.php
@@ -7,21 +7,16 @@
 get_header(); ?>
 
 
-<?php if ( have_posts() ) : ?>
+<div id="contact">
+    <?php if ( have_posts() ) : ?>
 
-    <?php while ( have_posts() ) : the_post(); ?>   
+        <?php while ( have_posts() ) : the_post(); ?>   
 
-        <?php the_content(); ?>
+            <?php the_content(); ?>
 
-    <?php endwhile; ?>
+        <?php endwhile; ?>
 
-<?php endif; ?>
-
-<div id="contactHero" style="background: url('https://trilakeguideservice.com/wp-content/uploads/2022/03/JoJo-100-e1646539030660.jpg') center top no-repeat; background-size: cover; height: auto; width: 100%;">
-
-    <div class="heroOverlay"></div>
-
-    <div class="logo"><img src="<?php the_sub_field('logo') ?>" /></div>
+    <?php endif; ?>
 
     <div class="heroContent">
 
@@ -30,7 +25,7 @@ get_header(); ?>
             <div class="heroHeadline animate__animated animate__fadeIn">
 
                 <div class="h2"><span class="underlined-word">Contact <div class="underline"></span></div></div>
-                <div class="h2 white"> Us</div>
+                <div class="h2 dark"> Us</div>
 
             </div>
 
