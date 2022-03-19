3edad043526c3e1f5d0a661cd7a0c8a35744962c
MrCrashocaster <mrcrashocaster@gmail.com>
2022-03-19 13:30:15 -0500

    Updated site.

--- about-page.php
+++ about-page.php
@@ -6,12 +6,45 @@
 
 get_header(); ?>
 
-<?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>    
-        
-            <?php the_content(); ?>
+<div id="about">
 
-        <?php endwhile; ?>
-<?php endif; ?>
+    <?php if ( have_posts() ) : ?>
+            <?php while ( have_posts() ) : the_post(); ?>    
+            
+                <?php the_content(); ?>
+
+            <?php endwhile; ?>
+    <?php endif; ?>
+
+    <div id="packagesHero" style="background: url('https://trilakeguideservice.com/wp-content/uploads/2022/03/JoJo-100-e1646539030660.jpg') center top no-repeat; background-size: cover; height: auto; width: 100%;">
+
+        <div class="heroOverlay"></div>
+
+        <div class="backButton">
+            <a onclick="history.back()">
+                <i class="fas fa-long-arrow-alt-left"></i>
+            </a>
+        </div>
+
+        <div class="logo"><img src="<?php the_sub_field('logo') ?>" /></div>
+
+        <div class="heroContent">
+
+            <div class="wrapper1200">
+
+                <div class="heroHeadline animate__animated animate__fadeIn">
+
+                    <div class="h2"><span class="underlined-word">About <div class="underline"></span></div></div>
+                    <div class="h2 white"> Us</div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
 
 <?php get_footer(); ?>
