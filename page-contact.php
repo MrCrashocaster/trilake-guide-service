67449bbe15850d50f3dd48554a22acde83e585a9
MrCrashocaster <mrcrashocaster@gmail.com>
2022-03-19 21:36:57 -0500

    Updated site.

--- page-contact.php
+++ page-contact.php
@@ -18,20 +18,33 @@ get_header(); ?>
 
     <?php endif; ?>
 
+    <div id="packagesHero" style="background: url('https://trilakeguideservice.com/wp-content/uploads/2022/03/Young_Man_Crappie-min.jpg') center top no-repeat; background-size: cover; height: auto; width: 100%;">
+
+    <div class="heroOverlay"></div>
+
+    <div class="backButton">
+        <a onclick="history.back()">
+            <i class="fas fa-long-arrow-alt-left"></i>
+        </a>
+    </div>
+
+    <div class="logo"><img src="<?php the_sub_field('logo') ?>" /></div>
+
     <div class="heroContent">
 
         <div class="wrapper1200">
 
             <div class="heroHeadline animate__animated animate__fadeIn">
 
-                <div class="h2"><span class="underlined-word">Contact <div class="underline"></span></div></div>
-                <div class="h2 dark"> Us</div>
+                <div class="h1">Contact <span class="red">Us.</span></div>
 
             </div>
 
         </div>
 
     </div>
+
+</div>
 
 </div>
 
