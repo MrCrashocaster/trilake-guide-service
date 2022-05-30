eba835f6b3e999fdaf23be6fc17d763212470f3f
MrCrashocaster <mrcrashocaster@gmail.com>
2022-05-29 23:52:27 -0500

    Updated site.

--- page-contact.php
+++ page-contact.php
@@ -18,86 +18,122 @@ get_header(); ?>
 
     <?php endif; ?>
 
-    <div id="packagesHero" style="background: url('https://trilakeguideservice.com/wp-content/uploads/2022/03/Young_Man_Crappie-min.jpg') center top no-repeat; background-size: cover; height: 100vh; width: 100%;">
+    <?php if( have_rows('hero') ): ?>
+        <?php while( have_rows('hero') ): the_row(); ?>
 
-        <div class="heroOverlay"></div>
+            <div id="packagesHero" style="background: url('<?php if(get_sub_field('background_image')['url']) { the_sub_field('background_image')['url']; } ?>') center top no-repeat; background-size: cover; height: 100vh; width: 100%;">
 
-        <div id="nav" class="nav">
-            <div class="wrapper1200">
-                <div class="navContent">
-                    <div class="navItems">
-                        <div class="singleItem">
-                            <a href="<?php echo get_home_url(); ?>" class="h4">Home</a>
-                        </div>
-                        <div class="singleItem">
-                            <a href="/packages/" class="h4">Packages</a>
-                        </div>
-                        <div class="singleItem">
-                            <a href="/about/" class="h4">About</a>
-                        </div>
-                        <div class="singleItem">
-                            <a href="/contact/" class="h4">Contact</a>
+                <div class="heroOverlay"></div>
+
+                <div id="nav" class="nav">
+                    <div class="wrapper1200">
+                        <div class="navContent">
+                            <div class="navItems">
+                                <div class="singleItem">
+                                    <a href="<?php echo get_home_url(); ?>" class="h4">Home</a>
+                                </div>
+                                <div class="singleItem">
+                                    <a href="/packages/" class="h4">Packages</a>
+                                </div>
+                                <div class="singleItem">
+                                    <a href="/about/" class="h4">About</a>
+                                </div>
+                                <div class="singleItem">
+                                    <a href="/contact/" class="h4">Contact</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div id="mobileMenu">
-            <div id="overlay-button"><span></span></div>
-            <div id="overlay">
-                <ul>
-                <li><a class="cwm" href="<?php echo get_home_url(); ?>">Home</a></li>
-                <li><a class="cwm" href="/packages/">Packages</a></li>
-                <li><a class="cwm" href="/about/">About</a></li>
-                <li><a class="cwm" href="/contact/">Contact</a></li>
-                </ul>
-            </div>
-        </div>
+                <div id="mobileMenu">
+                    <div id="overlay-button"><span></span></div>
+                    <div id="overlay">
+                        <ul>
+                        <li><a class="cwm" href="<?php echo get_home_url(); ?>">Home</a></li>
+                        <li><a class="cwm" href="/packages/">Packages</a></li>
+                        <li><a class="cwm" href="/about/">About</a></li>
+                        <li><a class="cwm" href="/contact/">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
 
-        <div class="logo"><img src="<?php the_sub_field('logo') ?>" /></div>
+                <div class="heroContent">
 
-        <div class="heroContent">
+                    <?php if( have_rows('headline') ): ?>
+                        <?php while( have_rows('headline') ): the_row(); ?>
 
-            <div class="wrapper1200">
+                            <div class="wrapper1200">
 
-                <div class="heroHeadline animate__animated animate__fadeIn">
+                                <div class="heroHeadline animate__animated animate__fadeIn">
 
-                    <div class="h1">Contact <span class="red">Us.</span></div>
+                                    <div class="h1"><?php if(get_sub_field('headline')) { the_sub_field('headline'); } ?> <span class="red"><?php if(get_sub_field('red_headline')) { the_sub_field('red_headline'); } ?></span></div>
+
+                                </div>
+
+                            </div>
+
+                        <?php endwhile; ?>
+
+                    <?php endif; ?>
+
+                    <div id="scrollButton" class="scrollButtonContainer">
+
+                    <img class="animate__animated animate__pulse animate__infinite infinite" src="https://trilakeguideservice.com/wp-content/uploads/2022/05/scroll.png"/>
+
+
+                </div>
 
                 </div>
 
             </div>
 
-            <div id="scrollButton" class="scrollButtonContainer">
+        <?php endwhile; ?>
 
-            <img class="animate__animated animate__pulse animate__infinite infinite" src="https://trilakeguideservice.com/wp-content/uploads/2022/05/scroll.png"/>
-
-
-        </div>
-
-        </div>
-
-    </div>
+    <?php endif; ?>
 
     <div id="contactContent">
-        <div class="wrapper1200">
-            <div class="contactContainer">
-                <div id="contactForm" class="contactForm">
-                    <div class="formHeadline animate__animated animate__fadeIn">
-                        <div class="h2 cwm">Get in <span class="red">Touch</span>.</div>
+        <?php if( have_rows('contact_information') ): ?>
+            <?php while( have_rows('contact_information') ): the_row(); ?>
+                <div class="wrapper1200">
+                    <div class="contactContainer">
+                        <div id="contactForm" class="contactForm">
+                            <?php if( have_rows('contact_form') ): ?>
+                                <?php while( have_rows('contact_form') ): the_row(); ?>
+                                    <?php if( have_rows('headline') ): ?>
+                                        <?php while( have_rows('headline') ): the_row(); ?>
+                                            <div class="formHeadline animate__animated animate__fadeIn">
+                                                <div class="h2 cwm"><?php if(get_sub_field('headline')) { the_sub_field('headline'); } ?> <span class="red"><?php if(get_sub_field('red_headline')) { the_sub_field('red_headline'); } ?></span></div>
+                                            </div>
+                                        <?php endwhile; ?>
+                                    <?php endif; ?>
+                                    <?php echo do_shortcode('[gravityform id="1" title="false" description="false" ajax="true" tabindex="49" field_values="check=First Choice,Second Choice"]'); ?>
+                                <?php endwhile; ?>
+                            <?php endif; ?>
+                        </div>
+                        <div class="contactInfoContainer">
+                            <?php if( have_rows('contact_information_section') ): ?>
+                                <?php while( have_rows('contact_information_section') ): the_row(); ?>
+                                    <?php if( have_rows('contact_information_section_headline') ): ?>
+                                        <?php while( have_rows('contact_information_section_headline') ): the_row(); ?>
+                                            <div class="h2"><span class="underlined-word"><?php if(get_sub_field('underlined_headline')) { the_sub_field('underlined_headline'); } ?><div class="underline"></span></div></div>
+                                            <div class="h2 dark subtext"> <?php if(get_sub_field('headline')) { the_sub_field('headline'); } ?></div>
+                                        <?php endwhile; ?>
+                                    <?php endif; ?>
+                                    <?php if( have_rows('contact_information') ): ?>
+                                        <?php while( have_rows('contact_information') ): the_row(); ?>
+                                            <p class="dark">Location: <?php if(get_sub_field('location')) { the_sub_field('location'); } ?></p>
+                                            <p class="dark">Phone: <a href="tel:<?php if(get_sub_field('phone')) { the_sub_field('phone'); } ?>" class="dark"><?php if(get_sub_field('phone')) { the_sub_field('phone'); } ?></a></p>
+                                            <p class="dark">Email: <a href="mailto:<?php if(get_sub_field('email')) { the_sub_field('email'); } ?>" class="dark"><?php if(get_sub_field('email')) { the_sub_field('email'); } ?></a></p>
+                                        <?php endwhile; ?>
+                                    <?php endif; ?>
+                                <?php endwhile; ?>
+                            <?php endif; ?>
+                        </div>
                     </div>
-                    <?php echo do_shortcode('[gravityform id="1" title="false" description="false" ajax="true" tabindex="49" field_values="check=First Choice,Second Choice"]'); ?>
                 </div>
-                <div class="contactInfoContainer">
-                    <div class="h2"><span class="underlined-word">Contact<div class="underline"></span></div></div>
-                    <div class="h2 dark subtext"> Info</div>
-                    <p class="dark">Location: Calhoun, MS</p>
-                    <p class="dark">Phone: <a href="tel:662.230.4223" class="dark">662.230.4223</a></p>
-                    <p class="dark">Email: <a href="mailto:jojobaker777@yahoo.com" class="dark">jojobaker777@yahoo.com</a></p>
-                </div>
-            </div>
-        </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </div>
 
 </div>
